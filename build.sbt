29405b6e64397eb253d26cf134dd31cebf9cf186
rocknrollMarc <marc.bluemner@gmail.com>
2014-03-01 16:46:56 +0100

    CRUD functionality

--- build.sbt
+++ build.sbt
@@ -4,6 +4,7 @@ scalaVersion := "2.10.3"
 
 organization := "Scala in Action"
 
+libraryDependencies += "org.specs2" %% "specs2" % "1.13" % "test"
 
 // append options Scala compiler
 scalacOptions ++= Seq("-deprecation", "-unchecked")
--- src/DBCollection.scala
+++ src/DBCollection.scala
@@ -3,6 +3,8 @@ package com.scalainaction.mongo
 import com.mongodb.{DBCollection => MongoDBCollection}
 import com.mongodb.DBObject
 
+class DBCollection(override val underlying: MongoDBCollection) extends ReadOnly
+
 trait ReadOnly {
 
   val underlying: MongoDBCollection
@@ -13,4 +15,14 @@ trait ReadOnly {
   def findOne(doc: DBObject) = underlying findOne doc
   def findOne = underlying findOne
   def getCount(doc: DBObject) = underlying getCount doc
+}
+
+trait Administrable extends ReadOnly {
+  def drop: Unit = underlying drop
+  def dropIndexes: Unit = underlying dropIndexes
+}
+
+trait Updatable extends ReadOnly {
+  def -=(doc: DBObject): Unit = underlying remove doc
+  def +=(doc: DBObject): Unit = underlying save doc
 }
